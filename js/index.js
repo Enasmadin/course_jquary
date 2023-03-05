5fa0ee253c6442688819032165806fca3c8687c2
enasmadina <enasmadina4@gmail.com>
2023-03-05 03:07:32 +0200

    three-lesson

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
 //    $("p").hide() ;
-$("p").css("color","red");
+// $("p").css("color","red");
+// $("button").click(function () {
+//     $("p").css("color","red"); 
+//     $(this).css("color","red");
+// })
+$("button").dblclick(function () {
+    $(this).css("color","red");  
+})
+$("button").mouseenter(function () {
+    $("p").css("color","red");
+})
+$("button").mouseleave(function () {
+    $("p").css("color","red"); 
+})
+$("button").hover(function () {
+    $("p").css("color","red"); 
+},
+function () {
+    $("p").css("color","blue");   
+}
+)
 })
