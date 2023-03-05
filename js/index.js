8db7fb9da0d337d5294598e6fcc10c6b5f287c07
enasmadina <enasmadina4@gmail.com>
2023-03-05 21:49:16 +0200

    lesson-4

--- js/index.js
+++ js/index.js
@@ -5,20 +5,39 @@ $(document).ready(function() {
 //     $("p").css("color","red"); 
 //     $(this).css("color","red");
 // })
-$("button").dblclick(function () {
-    $(this).css("color","red");  
+
+//  event in jquary 
+// $("button").dblclick(function () {
+//     $(this).css("color","red");  
+// })
+// $("button").mouseenter(function () {
+//     $("p").css("color","red");
+// })
+// $("button").mouseleave(function () {
+//     $("p").css("color","red"); 
+// })
+ 
+// hover comit to mouseenter and mouseleave 
+
+// $("button").hover(function () {
+//     $("p").css("color","red"); 
+// },
+// function () {
+//     $("p").css("color","blue");   
+// }
+// )
+//  effect in jquary 
+
+$("button").click(function() {
+    //  $("p").hide(5000,function () {
+    //     $("span").css("color","red");
+    //  });
+     $("p").toggle(5000,function () {
+        $("span").css("color","red");
+        $("button").hide();
+     });
+    // $("span").toggle();
+
 })
-$("button").mouseenter(function () {
-    $("p").css("color","red");
-})
-$("button").mouseleave(function () {
-    $("p").css("color","red"); 
-})
-$("button").hover(function () {
-    $("p").css("color","red"); 
-},
-function () {
-    $("p").css("color","blue");   
-}
-)
+
 })
